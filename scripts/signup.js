5830a95a1c955e5634baacdc8725ad4033997e3f
Mariam Hasanat <hasanattmariam@gmail.com>
2024-12-05 15:40:58 +0200

    feat: Initialize Firebase

---
+++ scripts/signup.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDMOlVGRnfwTCa83YpF4gUpbYYMu4jMnBA",
+    authDomain: "study-room-application.firebaseapp.com",
+    projectId: "study-room-application",
+    storageBucket: "study-room-application.firebasestorage.app",
+    messagingSenderId: "102691908238",
+    appId: "1:102691908238:web:b4d54c3fb01d5e0ca077df",
+    measurementId: "G-DW46LFLGB3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
