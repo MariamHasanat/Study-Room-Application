b8fae33136f02019b15b9e01cc201dabeb138ebc
Mariam Hasanat <hasanattmariam@gmail.com>
2024-12-08 20:08:38 +0200

    edit and update style

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
 const userData = JSON.parse(localStorage.getItem('userName'));
 
-const heading = document.querySelector('h1');
+const heading = document.querySelector('article');
 
 heading.innerHTML = heading.textContent + userData.name;
