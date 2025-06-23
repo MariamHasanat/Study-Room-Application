54964d3e87675f1e6d34b6d0b43aaedd46136e91
barahaw <barahawamdeh2003@gmail.com>
2025-06-23 20:40:10 +0300

    Configure Firebase settings

---
+++ scripts/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsZ1kECnwN7UeR3Z30faVC0KcCs1U3D5E",
+  authDomain: "study-room-ea8ca.firebaseapp.com",
+  projectId: "study-room-ea8ca",
+  storageBucket: "study-room-ea8ca.firebasestorage.app",
+  messagingSenderId: "35516750398",
+  appId: "1:35516750398:web:8edd38791d1213d00c0f6f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
