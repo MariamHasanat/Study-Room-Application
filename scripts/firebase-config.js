f4f10a43c6426052b51af91914d60c8cd765bb54
Shadi Sbaih <shadisbaih20014@gmail.com>
2025-06-23 19:26:02 +0300

    init firebase

---
+++ scripts/firebase-config.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCBo_QW_51HsxGhtgxul5Rig5vE2H6IRCw",
+    authDomain: "study-room-firebase-bootcamp.firebaseapp.com",
+    projectId: "study-room-firebase-bootcamp",
+    storageBucket: "study-room-firebase-bootcamp.firebasestorage.app",
+    messagingSenderId: "210580883766",
+    appId: "1:210580883766:web:f8f491b4bf4eadbd784b97"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
