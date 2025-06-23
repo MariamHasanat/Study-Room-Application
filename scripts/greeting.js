e1b07f297d224509332d169c20f7985393f0f786
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2025-06-23 19:03:02 +0300

    new repo

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -6,6 +6,7 @@ export function setGreeting(greetingDiv, userData) {
         if (hour < 12) greet = "Good morning";
         else if (hour < 18) greet = "Good afternoon";
         else greet = "Good evening";
+        
         greetingDiv.textContent = `${greet}, ${userData.name} 👋🏆!`;
     }
 }
