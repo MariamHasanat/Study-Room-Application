f2cf097d8c59bdd1f9870f0fd283af090df2e967
hosam221 <hosamalasham33@gmail.com>
2025-06-24 15:31:03 +0300

    add firebase-config.js file

---
+++ scripts/firebase-config.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD6IyvaPs8AwFqyzB2LpZaeLfu3us2VnT0",
+    authDomain: "husam-salah.firebaseapp.com",
+    projectId: "husam-salah",
+    storageBucket: "husam-salah.firebasestorage.app",
+    messagingSenderId: "972920186008",
+    appId: "1:972920186008:web:89bb829ca2f5781b0f9ced"
+  };
+
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
