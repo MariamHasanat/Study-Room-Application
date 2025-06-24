a5840e2daafa5b3513de76eb5217313babc85c9e
Aws Nassar <aws.a.nassar@gmail.com>
2025-06-24 06:35:20 +0300

    Add firebase-config.js file

---
+++ scripts/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyCyoyw60KfnefTRkz-82dW4wh_lY_3D1-I",
+authDomain: "fir-bootcamp-ad71b.firebaseapp.com",
+projectId: "fir-bootcamp-ad71b",
+storageBucket: "fir-bootcamp-ad71b.firebasestorage.app",
+messagingSenderId: "400595886747",
+appId: "1:400595886747:web:675535c44d47b5550e1c3e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
