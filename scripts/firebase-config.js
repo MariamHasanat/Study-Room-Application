6f9d1e3bd426e97714ef253540327971d8e1f680
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-06-24 11:18:18 +0300

    feat: add Firebase configuration and initialization script

---
+++ scripts/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ5F0wnOn0kyiBWN_SAMqKY1RlqYXXxks",
+  authDomain: "fir-bootcamp-afb03.firebaseapp.com",
+  projectId: "fir-bootcamp-afb03",
+  storageBucket: "fir-bootcamp-afb03.firebasestorage.app",
+  messagingSenderId: "58876289845",
+  appId: "1:58876289845:web:3dde7f92269724c802e27c",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
