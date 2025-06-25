0412fe5b65460d55f11db19a49834f56d63c29b4
Mohammed Qashqesh <mo.qashqesh@gmail.com>
2025-06-25 18:31:46 +0300

    connect firebase with program

---
+++ scripts/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDl8W9K485WycSBDhm4QXEFGSxOLyjsF3E",
+  authDomain: "fir-bootcamp-e7307.firebaseapp.com",
+  projectId: "fir-bootcamp-e7307",
+  storageBucket: "fir-bootcamp-e7307.firebasestorage.app",
+  messagingSenderId: "277134616980",
+  appId: "1:277134616980:web:3e6791a0e745e774bac781",
+  measurementId: "G-HZ7YRHKPWC",
+};
+
+export const app = initializeApp(firebaseConfig);
