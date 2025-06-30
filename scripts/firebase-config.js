0b2e287a3c3f6d9fd692915736d545cd55f80c86
B A S I L <107409699+Basil-Aladra@users.noreply.github.com>
2025-06-30 19:22:23 +0300

    configuration database & firestore

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -3,6 +3,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebas
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDd12tytbQ9LHOhsEgxqkp1TOQyaFfDV2o",
@@ -11,7 +13,11 @@ const firebaseConfig = {
   storageBucket: "basilfierbase.firebasestorage.app",
   messagingSenderId: "4043824972",
   appId: "1:4043824972:web:87ca2a81c86066167279e9",
+  databaseURL: "https://basilfierbase-default-rtdb.firebaseio.com/",
+  measurementId: "G-3F5Z1V9K6E",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+export const firestore = getFirestore(app);
