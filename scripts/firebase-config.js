2c18ba272c98642e59ab4277766623c2175251c5
DimaSalha <dimagit2022@gmail.com>
2025-07-02 21:46:26 +0300

    Add Firestore and RTDB

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,8 +1,8 @@
 
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+  import { getFirestore  } from  "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+  import { getDatabase } from  "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -11,8 +11,11 @@
     projectId: "fir-bootcamp-ca11a",
     storageBucket: "fir-bootcamp-ca11a.firebasestorage.app",
     messagingSenderId: "1028489722774",
-    appId: "1:1028489722774:web:d6b66fdae52ef80d0efbb7"
+    appId: "1:1028489722774:web:d6b66fdae52ef80d0efbb7",
+    databaseURL: "https://fir-bootcamp-ca11a-default-rtdb.firebaseio.com/",
   };
 
   // Initialize Firebase
   export const app = initializeApp(firebaseConfig);
+  export const firestore = getFirestore(app);
+  export const database = getDatabase(app);
