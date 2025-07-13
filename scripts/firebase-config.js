a81a16f8554d164e5ca608e36b920c4fdf02ac9e
Aws Nassar <aws.a.nassar@gmail.com>
2025-07-13 12:05:05 +0300

    Added Firestore and Real-DB

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +12,11 @@ authDomain: "fir-bootcamp-ad71b.firebaseapp.com",
 projectId: "fir-bootcamp-ad71b",
 storageBucket: "fir-bootcamp-ad71b.firebasestorage.app",
 messagingSenderId: "400595886747",
-appId: "1:400595886747:web:675535c44d47b5550e1c3e"
+appId: "1:400595886747:web:675535c44d47b5550e1c3e",
+databaseURL: "https://fir-bootcamp-ad71b-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const database = getDatabase(app);
